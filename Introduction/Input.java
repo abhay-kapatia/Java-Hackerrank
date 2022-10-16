ffe72eb80481fe87cd5f994b6e993947c1f1556f
abhay-kapatia <abhay2001kapatia@gmail.com>
2022-10-16 13:33:26 +0530

    16/10

---
+++ Introduction/Input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Input{
+	public static void main (String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		System.out.println(a);
+	}	
+}
+
+//nextBoolean() 	Reads a boolean value from the user
+//nextByte() 	Reads a byte value from the user
+//nextDouble() 	Reads a double value from the user
+//nextFloat() 	Reads a float value from the user
+//nextInt() 	Reads a int value from the user
+//nextLine() 	Reads a String value from the user
+//nextLong() 	Reads a long value from the user
+//nextShort() 	Reads a short value from the user
